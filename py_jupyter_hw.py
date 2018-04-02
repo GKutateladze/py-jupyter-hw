b363cc87c68fff47182581f1a4e8d3253771cf2f
GeorgKutateladze <georgerailz@gmail.com>
2018-04-02 23:25:47 +0300

    Первая часть домашки по jupyter notebook. При заполнении списка years от 1880 до 2016 года выводит самые распространенные имена как для одного года, так и для нескольких. Вторая часть. Выводит таблицу с суммарным количеством женщин и мужчин за указанные годы. Список лет можно увеличивать или уменьшать.

--- py_jupyter_hw.py
+++ py_jupyter_hw.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import os
-
+# ------- PART1 -------
 def common_name(year):
     data_path = f'{os.getcwd()}\\names\\'
     names = pd.read_csv(data_path + 'yob' + str(year) + '.txt', names=['name', 'gender', 'count'])
@@ -15,3 +15,18 @@ def most_common_name(years_list):
 
 years = [1900, 1950, 2000]
 print(most_common_name(years))
+
+# ------- PART2 -------
+def name_count(year):
+    data_path = f'{os.getcwd()}\\names\\'
+    names = pd.read_csv(data_path + 'yob' + str(year) + '.txt', names=['name', 'gender', 'count'])
+    return [names[names.gender == 'F']['count'].sum(), names[names.gender == 'M']['count'].sum()]
+
+def name_dynamics(year_list):
+    data = list(map(name_count, year_list))
+    dynamics = pd.DataFrame(data, index = year_list)
+    dynamics.columns = ['Female', 'Male']
+    return dynamics
+
+years = [1900, 1950, 2000]
+print(name_dynamics(years))
